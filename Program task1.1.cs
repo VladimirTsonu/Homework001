003afc48c8a7b12277368ad29810b3b4cdc2b3f0
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-11 01:42:55 +0500

    Add files via upload

---
+++ Program task1.1.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter first number: ");
+int firstNumb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumb = Convert.ToInt32(Console.ReadLine());
+if (firstNumb > secondNumb)
+{
+    Console.WriteLine("maximum number is: " + firstNumb );
+}
+else
+{
+    Console.WriteLine("maximum number is: " + secondNumb );
+}
