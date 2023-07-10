a5ec282365e4cd31d580de9c2702d488d6973620
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-11 01:44:36 +0500

    Add files via upload

---
+++ Program task1.2.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter first number: ");
+int firstNumb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter thrid number: ");
+int thridNumb = Convert.ToInt32(Console.ReadLine());
+int maxNumb = firstNumb;
+
+if (secondNumb > maxNumb)
+{
+    maxNumb = secondNumb;
+}
+if (thridNumb > maxNumb)
+{
+    maxNumb = thridNumb;
+}
+
+Console.WriteLine("maximum number is: " + maxNumb );
+
