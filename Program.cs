b0377f44e248e47859a4b7133fe93bdd63fac560
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-11 01:52:06 +0500

    Add files via upload
    task n3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.Write("your number is even");
+}
+else
+{
+    Console.Write("your number is not even");
+}
