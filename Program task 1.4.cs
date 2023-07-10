2cc0c51ebd1ce0fc396290b6e8fb9db547e265a3
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-11 01:53:40 +0500

    Add files via upload

---
+++ Program task 1.4.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter your N number: ");
+int nNumber = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i <= nNumber)
+{
+    Console.Write(i + ",");
+     i = i + 2;
+}
+
