93d2bd533465f54b44870eb6a2f71e3227a062a8
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-14 21:06:57 +0500

    Add files via upload

---
+++ Program 14-07 23.1.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DaysOfWeek (int number)
+{
+    if (number / 5 == 0 || number % 5 == 0)
+    Console.WriteLine($"Your number {number} is work day");
+    else if (number % 5 > 0)
+    Console.WriteLine($"Your number {number} is day off");
+}
+
+ Console.WriteLine("Enter your number");
+ int user_number = Convert.ToInt32(Console.ReadLine());
+
+ if (user_number > 0 && user_number < 8)
+    DaysOfWeek(user_number);
+    else
+    Console.WriteLine("Your number is not correct");
