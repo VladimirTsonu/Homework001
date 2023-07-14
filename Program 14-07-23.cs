5e039e4153128641186ab2ea005487b9275f7c65
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-14 21:03:10 +0500

    Add files via upload

---
+++ Program 14-07-23.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int MiddleDigit (int number)
+{
+    int removeHun = number % 100;
+    int dec = removeHun / 10;
+    return dec;
+}
+
+Console.WriteLine("Enter your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 99 && user_number < 1000)
+{
+    int result = MiddleDigit(user_number);
+    Console.WriteLine($"Your middle number is: {result}");
+}
+else
+{
+    Console.WriteLine("Your number is not 3digits");
+}
