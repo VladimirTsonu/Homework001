b15f70b5ce12089dc3bdfb99359aea3c5ab5c47f
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-14 21:02:09 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿Console.Write("Enter your number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if(number % 2 == 0)
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int MiddleDigit (int number)
 {
-    Console.Write("your number is even");
+    int removeHun = number % 100;
+    int dec = removeHun / 10;
+    return dec;
+}
+
+Console.WriteLine("Enter your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 99 && user_number < 1000)
+{
+    int result = MiddleDigit(user_number);
+    Console.WriteLine($"Your middle number is: {result}");
 }
 else
 {
-    Console.Write("your number is not even");
+    Console.WriteLine("Your number is not 3digits");
 }
